0afd535e20dbf55b6daec7bd0d7549eef04ea9c1
Matheus <maatheuscamargo1@gmail.com>
2025-09-03 16:42:30 -0300

    projeto 80%

---
+++ src/Components/Home.jsx
@@ -0,0 +1,14 @@
+
+import Feed from './Feed/Feed'
+
+
+const Home = () => {
+  return (
+    <section className='container mainContainer'>
+      <Feed/>
+    </section>
+  )
+}
+
+export default Home
+
---
+++ src/Components/Login/LoginPasswordLostReset.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginPasswordLostReset = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LoginPasswordLostReset
