32a96928d8900174cdb39d6bc31404d7189d6329
Matheus <maatheuscamargo1@gmail.com>
2025-09-03 18:06:33 -0300

    ultimo 03/09

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -6,16 +6,18 @@ import Error from "../Helper/Error";
 import Loading from "../Helper/Loading";
 import styles from "./FeedPhotos.module.css";
 
-const FeedPhotos = ({ user, setModalPhoto }) => {
+const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
   const { data, loading, error, request } = useFetch();
 
-React.useEffect(() => {
-  async function fetchPhotos() {
-    const { url, options } = PHOTOS_GET(1, 3, user); // <-- aqui ajusta
-    await request(url, options);
-  }
-  fetchPhotos();
-}, [request, user]);
+  React.useEffect(() => {
+    async function fetchPhotos() {
+      const total = 3;
+      const { url, options } = PHOTOS_GET(page, total, user); // <-- aqui ajusta
+      const { response, json } = await request(url, options);
+      if (response && response.ok && json.length < total) setInfinite(false);
+    }
+    fetchPhotos();
+  }, [request, user, page, setInfinite]);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
